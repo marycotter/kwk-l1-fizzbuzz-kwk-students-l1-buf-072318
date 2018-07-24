387efcba5d8089f5f00de22f6e23de874a9ae181
Mary Cotter <marebearc@gmail.com>
2018-07-24 18:56:10 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 
 
 def fizzbuzz(number)
-  if number % 3==0 
+  if number % 3==0 && number % 5==0
+    "fizzbuzz"
+  elsif number % 3==0 
     "fizz"
   elsif number % 5==0
     "buzz"
-  if number % 3==0 && number % 5==0
-    "fizzbuzz"
   end
   
 end
